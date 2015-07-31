8b450c53bc4f81230a254fdf32775b02ef1e4029
Patrick Farrell <pfarrell@prosper.com>
2015-07-30 17:58:52 -0700

    initial commit

---
+++ app.rb
@@ -0,0 +1,13 @@
+$: << File.expand_path('../app', __FILE__)
+
+require 'sinatra'
+require 'haml'
+
+class App < Sinatra::Application
+  enable :sessions
+  set :session_secret, ENV["APP_SESSION_SECRET"] || "youshouldreallychangethis"
+  set :views, Proc.new { File.join(root, "app/views") }
+end
+
+require 'models'
+require 'routes'
